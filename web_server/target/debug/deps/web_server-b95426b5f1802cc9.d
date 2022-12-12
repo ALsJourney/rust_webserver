04fcc5efa0d6cf27dcc7e6916cbfc338dd60d497
ALsJourney <aldin.dino@outlook.com>
2022-12-12 23:04:36 +0100

    Stopped at reading the request

---
+++ web_server/target/debug/deps/web_server-b95426b5f1802cc9.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/aldin/OneDrive/Desktop/rust_webserver/web_server/target/debug/deps/web_server-b95426b5f1802cc9: src/main.rs
+
+/mnt/c/Users/aldin/OneDrive/Desktop/rust_webserver/web_server/target/debug/deps/web_server-b95426b5f1802cc9.d: src/main.rs
+
+src/main.rs:
---
+++ web_server/target/debug/web_server.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/aldin/OneDrive/Desktop/rust_webserver/web_server/target/debug/web_server: /mnt/c/Users/aldin/OneDrive/Desktop/rust_webserver/web_server/src/main.rs
